88639221e51777e0e32c812cd800e187f1acdcb7
irtiza.rizvi <irtiza.rizvi@rightstar.com>
2017-03-05 05:20:31 -0500

    explaining the lights out variant in the help command

--- src/com/ttt/game/command/HelpCommand.java
+++ src/com/ttt/game/command/HelpCommand.java
@@ -12,7 +12,7 @@ public class HelpCommand extends Command {
 				+ "(ie 'start x 3' starts a game where you are X on a 3x3 board, 'start o 5' starts a game where you are O on a 5x5 board, etc).  Just typing 'start' will default to 'start x 3'.\n\n"
 				+ "To make a move, type the row and column number (ie '1,1' is the top-left corner, '1,3' is the top-right corner on a 3x3 board, etc).\n\n"
 				+ "To quit the current game, type 'quit', and at any point, you can exit the program by typing 'exit'.\n\n"
-				+ "For a special Tic Tac Toe variant, type 'start lightsout' :).\n\n"
+				+ "For a special Tic Tac Toe variant, type 'start lightsout' :).  The rules are the same, except when you pick your move, you also fill in the adjacent squares (top, bottom, left, right).  If an adjacent square already has your mark, nothing happens, and if it has your opponent's mark, then it goes blank.\n\n"
 				+ "Command: ");
 	}
 }
